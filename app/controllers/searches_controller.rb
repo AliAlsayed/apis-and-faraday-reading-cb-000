521e86e982e8e723f343128df48146d1a5305d84
Ali Alsayed <a_alsayed87@hotmail.com>
2019-08-22 09:06:02 +0000

    Automatically backed up by Learn

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -6,6 +6,7 @@ class SearchesController < ApplicationController
     Faraday.get 'https://api.foursquare.com/v2/venues/search' do |req|
       req.params['client_id'] = '1W211QEWBVU50BUP5RILVJFTKC51KOL2LX3FDULXJYY4O2CK'
       req.params['client_secret'] = 'L23ZPZSTQIHAKH1XR4MNSAL4XUD1IWNRCS4PQGWCW5W4CBIY'
+      req.params['v'] = '20160201'
     end
   end
 end
