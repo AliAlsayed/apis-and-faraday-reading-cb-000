b098274c01e8a5463373144d95aac32f7ef9a47f
Ali Alsayed <a_alsayed87@hotmail.com>
2019-08-22 09:07:02 +0000

    Automatically backed up by Learn

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -7,6 +7,8 @@ class SearchesController < ApplicationController
       req.params['client_id'] = '1W211QEWBVU50BUP5RILVJFTKC51KOL2LX3FDULXJYY4O2CK'
       req.params['client_secret'] = 'L23ZPZSTQIHAKH1XR4MNSAL4XUD1IWNRCS4PQGWCW5W4CBIY'
       req.params['v'] = '20160201'
+      req.params['near'] = params[:zipcode]
+      req.params['query'] = 'coffee shop'
     end
   end
 end
